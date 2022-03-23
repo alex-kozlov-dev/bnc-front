de5d95a2dbe0d0ba85258e8ed9fe2a034cd99e0d
Alex Kozlov <alex.k@preply.com>
2022-03-23 15:25:50 +0200

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@900&display=optional" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,900;1,900&display=optional" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=optional" rel="stylesheet"/>
         </Head>
         <body>
