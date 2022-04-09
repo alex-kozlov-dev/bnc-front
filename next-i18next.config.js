20fc7f63c53206829ce78e9f08dff4d940beb923
Alex Kozlov <alex.k@preply.com>
2022-04-09 21:39:17 +0300

    fix broken preview

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en', 'uk'],
     defaultLocale: 'uk',
+    localePath: path.resolve('./public/locales'),
     localeDetection: false
   }
 }
