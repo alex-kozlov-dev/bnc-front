cbd3e8b1b209a2c446316539a66bfa76cac03b40
Alex Kozlov <alex.k@preply.com>
2022-04-09 21:21:37 +0300

    disable maintenance mode in preview

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -3,8 +3,11 @@ import { NextMiddleware, NextResponse } from 'next/server'
 const isMaintenance = process.env.MAINTENANCE === 'true'
 
 export const middleware: NextMiddleware = (req) => {
-  console.log(process.env.MAINTENANCE)
-  if (isMaintenance && req.nextUrl.pathname === '/') {
+  if (
+    isMaintenance &&
+    req.nextUrl.pathname === '/' &&
+    !req.cookies.__next_preview_data
+  ) {
     const url = req.nextUrl.clone()
     url.pathname = '/coming-soon'
     return NextResponse.redirect(url)
